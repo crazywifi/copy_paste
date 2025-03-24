739df2d34f425ff0e3ab605dab8f3aebbd763647
LazyHacker <rishabh2241991@gmail.com>
2025-03-24 09:41:25 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,2 +1,1 @@
-connectionpool.py:1099: InsecureRequestWarning: Unverified HTTPS request is being made to host 'apim.people.deloitte'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/latest/advanced-usage.html#tls-warnings
-  warnings.warn(
+a
